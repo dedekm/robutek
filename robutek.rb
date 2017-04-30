46390f13ee497debf75bcb1bc78e3972ecebb629
Martin Dedek <martin.dedek@sinfin.cz>
2017-04-30 12:59:50 +0200

    INIT + gemfile, robutek.rb

---
+++ robutek.rb
@@ -0,0 +1,37 @@
+require 'dino'
+
+class Robutek
+  @base
+  @board
+  @stepperL
+  @stepperR
+  
+  def initialize( base )
+    @base = base
+    loop do
+      e = false
+      begin
+          @board = Dino::Board.new(Dino::TxRx::Serial.new)
+      rescue Dino::BoardNotFound => e
+          puts 'No board found!'
+          puts '...'
+          sleep 0.5
+      end
+      break if !e
+    end
+    puts 'Board found and connected!'
+  end
+  
+  def setLeftStepper( step, dir )
+    @stepperL = Dino::Components::Stepper.new(board: @board, pins: { step: step, direction: dir })
+  end
+  def setRightStepper( step, dir )
+    @stepperR = Dino::Components::Stepper.new(board: @board, pins: { step: step, direction: dir })
+  end
+end
+
+robutek = Robutek.new 400
+robutek.setLeftStepper 12, 10
+robutek.setRightStepper 4, 2
+
+puts 'DONE'
